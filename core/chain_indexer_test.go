e8a5668722f0dd32e2137586f38c8016219cf526
owenSai <33215096+owenSai@users.noreply.github.com>
2024-09-19 14:54:54 +0800

    import some error strings

--- core/chain_indexer_test.go
+++ core/chain_indexer_test.go
@@ -224,7 +224,10 @@ func (b *testChainIndexBackend) Process(ctx context.Context, header *types.Heade
 	//t.processCh <- header.Number.Uint64()
 	select {
 	case <-time.After(10 * time.Second):
-		b.t.Fatal("Unexpected call to Process")
+		b.t.Error("Unexpected call to Process")
+		// Can't use Fatal since this is not the test's goroutine.
+		// Returning error stops the chainIndexer's updateLoop
+		return errors.New("unexpected call to Process")
 	case b.processCh <- header.Number.Uint64():
 	}
 	return nil
